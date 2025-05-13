c8deb0a8ea39f8a3e49a61af135e2e57913589de
Paras Waral <paraswaral2@gmail.com>
2025-05-13 08:23:30 +0530

    Added code files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");/
+
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Ram ram bhaisare");
+        }
+    }
+}
